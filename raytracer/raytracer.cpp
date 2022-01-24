603d913bc16833cad99ae2b73c0f9704f4a271bf
guilh <guilh@DELL-GUI>
2022-01-23 23:13:55 -0300

    Adicionar arquivos de projeto.

---
+++ raytracer/raytracer.cpp
@@ -0,0 +1,12 @@
+﻿// raytracer.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "raytracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
