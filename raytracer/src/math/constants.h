93c83d51f84f943d5578756edc67c954fb56fd91
Guilherme Lujan <guilhermelucaslujan@gmail.com>
2023-12-03 13:56:08 -0300

    remove old files

--- raytracer/src/math/constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CONSTANTS_H_
-#define _CONSTANTS_H_
-
-namespace Constants {
-	constexpr auto PI = 3.14159265358979323846f;
-}
-
-#endif
--- raytracer/src/math/float_operations.cpp
+++
@@ -1,5 +0,0 @@
-#include <cmath>
-#include "float_operations.h"
-namespace floats {
-	bool equal(float a, float b) { return abs(a - b) < EPSILON; }
- }
